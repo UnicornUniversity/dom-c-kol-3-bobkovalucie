ed34863b42f3bf9a1dfaab863fb813d33c4eb6c6
bobkovalucie <bobkova.luccka@gmail.com>
2025-11-24 21:59:28 +0100

    update - function generateName, generateSurname

--- main.js
+++ main.js
@@ -33,46 +33,72 @@ function setEmployeeGender() {
 }
 function generateName (gender){
     let nameNumber;
-    let surnameNumber;
-
     if(gender === "male"){
         const maleNames = [
-            "Jan", "Petr", "Josef", "Pavel", "Martin", "Tomáš", "Jaroslav", "Miroslav", "Jiří", "Karel", "Lukáš", "Václav", "Ondřej", "Michal", "Roman", "David", "Marek", "Radek", "Jakub", "Zdeněk",  "Stanislav", "František", "Aleš", "Daniel", "Libor", "Vladimír", "Filip", "Adam", "Richard", "Robert", "Štěpán", "Ladislav", "Vojtěch", "Bohumil", "Matěj", "Milan", "Patrik",
-            "Erik", "Jindřich", "Dominik", "Eduard", "Antonín", "Samuel", "Hynek", "Tadeáš", "Oldřich", "Rudolf", "Branislav", "Leoš"
-        ];
-        const maleSurnames = [
-            "Novák", "Svoboda", "Novotný", "Dvořák", "Černý", "Procházka", "Kučera", "Veselý", "Horák", "Němec", "Pospíšil", "Pokorný", "Hájek", "Jelínek",
-            "Růžička", "Beneš", "Fiala", "Sedláček", "Doležal", "Zeman", "Kolář", "Navrátil", "Čermák", "Vaněk", "Urban", "Král", "Bartoš", "Kovář", "Kříž", "Šťastný", "Nedvěd", "Malý", "Mach", "Šimek", "Konečný", "Slavík", "Bláha", "Moravec", "Zima",
-            "Štěpánek", "Bednář", "Bureš", "Holub", "Sýkora", "Ptáček", "Richter", "Toman", "Musil", "Vlček"
-        ];
-        nameNumber = getRandom(0,maleNames.length-1);
-        surnameNumber = getRandom(0,maleSurnames.length-1);
-        return {
-            name: maleNames[nameNumber],
-            surname: maleSurnames[surnameNumber]
-        }
+            "Jan", "Petr", "Josef", "Pavel", "Martin", "Tomáš", "Jaroslav",
+            "Miroslav", "Jiří", "Karel", "Lukáš", "Václav", "Ondřej", "Michal",
+            "Roman", "David", "Marek", "Radek", "Jakub", "Zdeněk", "Stanislav",
+            "František", "Aleš", "Daniel", "Libor", "Vladimír", "Filip", "Adam",
+            "Richard", "Robert", "Štěpán", "Ladislav", "Vojtěch", "Bohumil",
+            "Matěj", "Milan", "Patrik", "Rostislav", "Erik", "Jindřich",
+            "Dominik", "Eduard", "Antonín", "Samuel", "Hynek", "Tadeáš",
+            "Oldřich", "Rudolf", "Branislav", "Leoš" ];
 
+        nameNumber = getRandom(0,maleNames.length-1);
+        return maleNames[nameNumber];
     }
+
     else{
         const femaleNames = [
-            "Marie", "Jana", "Eva", "Anna", "Hana", "Lucie", "Kateřina", "Tereza", "Petra", "Lenka", "Veronika", "Markéta", "Monika", "Alena", "Barbora", "Klára", "Michaela", "Adéla", "Karolína", "Zuzana",
-            "Eliška", "Kristýna", "Daniela", "Ivana", "Martina", "Šárka", "Dagmar", "Božena", "Nikola", "Renata", "Gabriela", "Simona", "Irena", "Natálie", "Vendula", "Dominika", "Sandra", "Lada", "Radka",
-            "Emilie", "Sabina", "Věra", "Andrea", "Stela", "Růžena", "Laura", "Sofie", "Tamara", "Patricie"
+            "Marie", "Jana", "Eva", "Anna", "Hana", "Lucie", "Kateřina", "Tereza",
+            "Petra", "Lenka", "Veronika", "Markéta", "Monika", "Alena", "Barbora",
+            "Klára", "Michaela", "Adéla", "Karolína", "Zuzana", "Helena", "Eliška",
+            "Kristýna", "Daniela", "Ivana", "Martina", "Šárka", "Dagmar", "Božena",
+            "Nikola", "Renata", "Gabriela", "Simona", "Irena", "Natálie", "Vendula",
+            "Dominika", "Sandra", "Lada", "Radka", "Blanka", "Emilie", "Sabina", "Věra",
+            "Andrea", "Stela", "Růžena", "Laura", "Sofie", "Tamara"
         ]
-        const femaleSurnames = [
-            "Nováková", "Svobodová", "Novotná", "Dvořáková", "Černá", "Procházková", "Kučerová", "Veselá", "Horáková", "Němcová", "Marková", "Pospíšilová", "Pokorná", "Hájková", "Jelínková", "Růžičková", "Benešová", "Fialová", "Sedláčková", "Doležalová",
-            "Zemanová", "Kolářová", "Navrátilová", "Čermáková", "Vaňková", "Urbanová", "Králová", "Bartošová", "Kovářová", "Křížová", "Šťastná", "Nedvědová", "Malá", "Šimková", "Konečná", "Slavíková", "Bláhová", "Moravcová", "Zimová",
-            "Štěpánková", "Bednářová", "Burešová", "Holubová", "Sýkorová", "Ptáčková", "Richterová", "Tomanová", "Musilová", "Vlčková"
-        ];
         nameNumber = getRandom(0,femaleNames.length-1);
-        surnameNumber = getRandom(0,femaleSurnames.length-1);
-        return {
-            name: femaleNames[nameNumber],
-            surname: femaleSurnames[surnameNumber]
-        }
-
+        return femaleNames[nameNumber]
     }
 }
+
+function generateSurname (gender){
+    let surnameNumber;
+    if(gender === "male"){
+        const maleSurnames = [
+            "Novák", "Svoboda", "Novotný", "Dvořák", "Černý", "Procházka", "Kučera",
+            "Veselý", "Horák", "Němec", "Marek", "Pospíšil", "Pokorný", "Hájek",
+            "Jelínek", "Růžička", "Beneš", "Fiala", "Sedláček", "Doležal", "Zeman",
+            "Kolář", "Navrátil", "Čermák", "Vaněk", "Urban", "Král", "Bartoš",
+            "Kovář", "Kříž", "Šťastný", "Nedvěd", "Malý", "Mach", "Šimek",
+            "Konečný", "Slavík", "Bláha", "Moravec", "Zima", "Štěpánek", "Bednář",
+            "Bureš", "Holub", "Sýkora", "Ptáček", "Richter", "Toman", "Musil", "Vlček"
+        ];
+        surnameNumber = getRandom(0, maleSurnames.length-1);
+        return maleSurnames[surnameNumber];
+    }
+
+    else{
+        const femaleSurnames = [
+            "Nováková", "Svobodová", "Novotná", "Dvořáková", "Černá", "Procházková",
+            "Kučerová", "Veselá", "Horáková", "Němcová", "Marková", "Pospíšilová",
+            "Pokorná", "Hájková", "Jelínková", "Růžičková", "Benešová", "Fialová",
+            "Sedláčková", "Doležalová", "Zemanová", "Kolářová", "Navrátilová",
+            "Čermáková", "Vaňková", "Urbanová", "Králová", "Bartošová", "Kovářová",
+            "Křížová", "Šťastná", "Nedvědová", "Malá", "Machová", "Šimková", "Konečná",
+            "Slavíková", "Bláhová", "Moravcová", "Zimová", "Štěpánková", "Bednářová",
+            "Burešová", "Holubová", "Sýkorová", "Ptáčková", "Richterová", "Tomanová",
+            "Musilová", "Vlčková"
+        ];
+        surnameNumber = getRandom(0, femaleSurnames.length-1);
+        return femaleSurnames[surnameNumber]
+    }
+}
+
+
+
+
 
 function setWorkload(){
     return (getRandom(1,4)*10);
@@ -97,8 +123,8 @@ function generateEmployee(minAge,maxAge){
         workload: "",
     }
     employee.gender = setEmployeeGender();
-    employee.name = generateName(employee.gender).name;
-    employee.surname = generateName(employee.gender).surname;
+    employee.name = generateName(employee.gender);
+    employee.surname = generateSurname(employee.gender);
     employee.birthdate = generateBirthday(minAge, maxAge);
     employee.workload = setWorkload();
     return employee;
@@ -127,4 +153,3 @@ export function main(dtoIn) {
     return dtoOut;
 }
 
-console.log(main(dtoIn));
