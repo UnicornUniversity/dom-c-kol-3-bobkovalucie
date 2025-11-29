3547e949565b0ee4c8a5b7721039e456fdd7aba9
bobkovalucie <bobkova.luccka@gmail.com>
2025-11-29 20:27:19 +0100

    Update generatingFunctions.js

--- src/generatingFunctions.js
+++ src/generatingFunctions.js
@@ -3,7 +3,7 @@ import {
     femaleNames, 
     maleSurnames, 
     femaleSurnames 
-} from '.src/listsOfNames.js';
+} from '.src/listsOfNames.js'; 
 
 /**
  * Functions for generating employee data (Name, Surname, Birthdate, Workload, etc.).
